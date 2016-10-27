c96b24f21fd846a02097f690e338c8e964c5a180
Jose Escorche <josesco5@gmail.com>
2016-10-26 23:52:31 -0400

    Scroll to bottom after load chat messages

--- www/js/controllers/chats/detail.js
+++ www/js/controllers/chats/detail.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers')
-  .controller('ChatDetailCtrl', function($scope, $state, $stateParams, Auth, Chats) {
+  .controller('ChatDetailCtrl', function($scope, $state, $stateParams, $timeout, $ionicScrollDelegate, Auth, Chats) {
     if (!Auth.loggedIn()) {
       $state.go('login');
     }
@@ -7,8 +7,17 @@ angular.module('starter.controllers')
     $scope.chat = Chats.get($stateParams.chatId);
     $scope.messages = [];
 
+    // Scroll to the bottom of the messages list
+    var scrollBottom = function () {
+      $timeout(function() {
+        $ionicScrollDelegate.$getByHandle('messages').scrollBottom();
+      }, 500);
+    };
+
     Chats.getMessages($stateParams.chatId)
       .then(function(response) {
         $scope.messages = response.data;
+
+        scrollBottom();
       });
   });
